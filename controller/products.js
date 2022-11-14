cc1a9a12714f79d17a2635ed35930b36564dd370
wy-hash <wy.hash@gmail.com>
2022-11-14 14:58:45 +0900

    Create 단위테스트 작성

--- controller/products.js
+++ controller/products.js
@@ -1,1 +1,7 @@
-exports.createProduct = (req, res) => {};
+const productModel = require("../models/Product");
+
+exports.createProduct = (req, res, next) => {
+    console.log(req.body);
+    const createdProduct = productModel.create(req.body);
+    res.status(201).json(createdProduct);
+};
--- test/unit/products.test.js
+++ test/unit/products.test.js
@@ -1,7 +1,38 @@
 const productController = require("../../controller/products");
+const productModel = require("../../models/Product");
+const httpMocks = require("node-mocks-http");
+const newProduct = require("../../data/new-product.json");
+
+productModel.create = jest.fn();
+let req, res, next;
+beforeEach(() => {
+  req = httpMocks.createRequest();
+  res = httpMocks.createResponse();
+  next = null;
+});
+
 
 describe("Product Controller Create", () => {
+  beforeEach(() => { 
+    req.body = newProduct;
+  })
   it("should have a createProduct function", () => {
     expect(typeof productController.createProduct).toBe("function");
+  });
+  it("should call ProductModel.create", () => {
+    productController.createProduct(req, res, next);
+    expect(productModel.create).toBeCalledWith(newProduct);
+  });
+  it("should return 201 response code", () => {
+    productController.createProduct(req, res, next);
+    expect(res.statusCode).toBe(201);
+    expect(res._isEndCalled()).toBeTruthy();
+  });
+
+  it("should return json body in response", () => { 
+    productModel.create.mockReturnValue(newProduct);
+    productController.createProduct(req, res, next);
+    expect(res._getJSONData()).toStrictEqual(newProduct);
+
   });
 });
