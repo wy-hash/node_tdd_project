453b6da92c55d727638bce018abd6e93844bf513
wy-hash <wy.hash@gmail.com>
2022-11-16 14:40:59 +0900

    통합테스트 추가

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
-const express = require("express");
-const productRoutes = require("./routes.js");
-const mongoose = require("mongoose");
+const express = require('express');
+const productRoutes = require('./routes.js');
+const mongoose = require('mongoose');
 
 const PORT = 5000;
-const HOST = "127.0.0.1";
+const HOST = '127.0.0.1';
 const app = express();
 
 // DB 연결
 mongoose
   .connect(
-    "mongodb+srv://root:root@tdd.nzeehnt.mongodb.net/TDD_DB?retryWrites=true&w=majority",
+    'mongodb+srv://root:root@tdd.nzeehnt.mongodb.net/TDD_DB?retryWrites=true&w=majority',
     {
       useNewUrlParser: true,
     }
   )
-  .then(() => console.log("MongoDb Connected..."))
+  .then(() => console.log('MongoDb Connected...'))
   .catch((err) => console.log(err));
 
 // 미들웨어 등록
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use("/api/products", productRoutes);
+app.use('/api/products', productRoutes);
 
 // 서버 실행
 /*
@@ -35,6 +35,8 @@ app.listen(PORT, () => {
 });
 
 // 라우터 등록
-app.get("/", (req, res) => {
-  res.send("hi");
+app.get('/', (req, res) => {
+  res.send('hi');
 });
+
+module.exports = app;
--- test/unit/products.test.js
+++ test/unit/products.test.js
@@ -1,7 +1,7 @@
-const productController = require("../../controller/products");
-const productModel = require("../../models/Product");
-const httpMocks = require("node-mocks-http");
-const newProduct = require("../../data/new-product.json");
+const productController = require('../../controller/products');
+const productModel = require('../../models/Product');
+const httpMocks = require('node-mocks-http');
+const newProduct = require('../../data/new-product.json');
 
 productModel.create = jest.fn();
 let req, res, next;
@@ -11,32 +11,31 @@ beforeEach(() => {
   next = jest.fn();
 });
 
-
-describe("Product Controller Create", () => {
-  beforeEach(() => { 
+describe('Product Controller Create', () => {
+  beforeEach(() => {
     req.body = newProduct;
-  })
-  it("should have a createProduct function", () => {
-    expect(typeof productController.createProduct).toBe("function");
   });
-  it("should call ProductModel.create", async () => {
+  it('should have a createProduct function', () => {
+    expect(typeof productController.createProduct).toBe('function');
+  });
+  it('should call ProductModel.create', async () => {
     await productController.createProduct(req, res, next);
     expect(productModel.create).toBeCalledWith(newProduct);
   });
-  it("should return 201 response code", async () => {
+  it('should return 201 response code', async () => {
     await productController.createProduct(req, res, next);
     expect(res.statusCode).toBe(201);
     expect(res._isEndCalled()).toBeTruthy();
   });
 
-  it("should return json body in response", async () => { 
+  it('should return json body in response', async () => {
     productModel.create.mockReturnValue(newProduct);
     await productController.createProduct(req, res, next);
     expect(res._getJSONData()).toStrictEqual(newProduct);
   });
 
-  it("should handle errors", async () => {
-    const errorMessage = { message: "dexcription property missing" };
+  it('should handle errors', async () => {
+    const errorMessage = { message: 'description property missing' };
     const rejectedPromise = Promise.reject(errorMessage);
     productModel.create.mockReturnValue(rejectedPromise);
     await productController.createProduct(req, res, next);
